98960e03d839f4947e32ceb32374f1f0fd654eeb
blues <ya.blues@yandex.ru>
2021-05-05 14:15:15 +0400

    first try

---
+++ gerber_parser.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import os
+import re
+
+# Constant
+TREASH = ["Altium",
+          "Designer",
+          "Software"]
+
+REG1 = '(' + '|'.join(i + '|' + i.lower() + '|' + i.upper() for i in TREASH) + ')'
+TreashPatt = re.compile(REG1)
+
+EXTENCIONS = ["BRD",
+              "DRL",
+              "GB*",
+              "GT*"]
+
+REG2 = '(' + '|'.join(f'{i.replace("*","[A-Z]")}|{i.replace("*","[A-Z]").lower()}' for i in EXTENCIONS) + ')'
+ExtPatt = re.compile(REG2)
+
+
+def file_validator(name: str) -> bool:
+    if not os.path.isdir(os.path.join(os.curdir, name)):
+        if len(ExtPatt.findall(name.split('.')[-1])):
+            return True
+    return False
+
+
+files = [os.path.join(os.curdir, i)
+         for i in os.listdir(os.curdir) if file_validator(i)]
+
+os.mkdir(os.path.join(os.curdir, "BACK"))
+for name in files:
+    back = os.path.join(os.path.split(name)[0], "BACK", os.path.split(name)[1])
+    os.rename(name, back)
+    with open(name, 'w') as targ:
+        with open(back, 'r') as src:
+            for s in src.readlines():
+                if len(TreashPatt.findall(s)) == 0:
+                    targ.write(s)
+                else:
+                    print(f"Finded {TreashPatt.findall(s)[0]} in string {s}")
